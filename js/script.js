6dc92f964339c504d7bba1a643849f91f5ffc2a9
mixfolio <62788627+mixfolio@users.noreply.github.com>
2020-10-22 12:42:04 +0300

    upd 12:41

--- js/script.js
+++ js/script.js
@@ -6,28 +6,59 @@ window.addEventListener('DOMContentLoaded', () => {
 btnBurger.addEventListener('click', () => {
    sideBarLeft.classList.toggle('sidebar-left--show');
 });
-   // Tabs tabs.js
-const tabs = document.querySelectorAll('.tab'),
+   // // Tabs tabs.js
+// const tabs = document.querySelectorAll('.tab'),
+//    tabsContent = document.querySelectorAll('.tab-content'),
+//    tabsParent = document.querySelector('.tabs-list');
+// function hideTabContent() {
+//    tabsContent.forEach(item => {
+//       item.style.display = 'none';
+//    });
+//    tabs.forEach(tab => {
+//       tab.classList.remove('tab-active');
+//    });
+// }
+// function showTabContent(i = 0) {
+//    tabsContent[i].style.display = '';
+//    tabs[i].classList.add('tab-active');
+// }
+// hideTabContent();
+// showTabContent();
+
+// tabsParent.addEventListener('click', (event) => {
+//    const target = event.target;
+//    if (target && target.classList.contains('tab')) {
+//       tabs.forEach((item, i) => {
+//          if (target == item) {
+//             hideTabContent();
+//             showTabContent(i);
+//          }
+//       });
+//    }
+// });
+
+// Tabs tabs.js
+const tabs = document.querySelectorAll('.tab-2'),
    tabsContent = document.querySelectorAll('.tab-content'),
-   tabsParent = document.querySelector('.tabs-list');
+   tabsParent = document.querySelector('.tabs-list-2');
 function hideTabContent() {
    tabsContent.forEach(item => {
       item.style.display = 'none';
    });
    tabs.forEach(tab => {
-      tab.classList.remove('tab-active');
+      tab.classList.remove('tab-2-active');
    });
 }
 function showTabContent(i = 0) {
    tabsContent[i].style.display = '';
-   tabs[i].classList.add('tab-active');
+   tabs[i].classList.add('tab-2-active');
 }
 hideTabContent();
 showTabContent();
 
 tabsParent.addEventListener('click', (event) => {
    const target = event.target;
-   if (target && target.classList.contains('tab')) {
+   if (target && target.classList.contains('tab-2')) {
       tabs.forEach((item, i) => {
          if (target == item) {
             hideTabContent();
