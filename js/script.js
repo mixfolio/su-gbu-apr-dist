bf95d6114fff13e9b95cb2cd90cfd997d374e56a
mixfolio <62788627+mixfolio@users.noreply.github.com>
2021-03-31 15:36:56 +0300

    upd menu

--- js/script.js
+++ js/script.js
@@ -10,6 +10,14 @@ btnBurger.addEventListener('click', () => {
    sidebarBtn = sidebarRight.querySelector('#sidebarBtn');
 sidebarBtn.addEventListener('click', () => {
    sidebarRight.classList.toggle('show-menu');
+   // if (!sidebarRight.classList.contains('show-menu')){
+   //    sidebarBtn.classList.remove('icon-arr-prev');
+   //    sidebarBtn.classList.add('icon-arr-next');
+   // } else {
+   //    sidebarBtn.classList.remove('icon-arr-next');
+   //    sidebarBtn.classList.add('icon-arr-prev');
+   // }
+   
 });
    
 let date = new Date();
